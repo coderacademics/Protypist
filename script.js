9a0aedb1dbb16f36db0120e728890e7f67530d8c
Saikat <saikatmohanta43434@gmail.com>
2021-04-17 03:19:36 -0400

    Made the buttons correct

--- script.js
+++ script.js
@@ -35,3 +35,32 @@ document.addEventListener("keypress", function (event) {
         console.log("The no. of wrong typed letter are:"+wrong);
     }
 });
+// document.querySelector(".play").addEventListener("click",function()
+// {
+
+//     document.querySelector(".box").innerHTML=`<button class="pause btn " event="click"><i class="fas fa-pause"></i></button>`;
+    
+// });
+// document.querySelector(".pause").addEventListener("click",function()
+// {
+    
+//     document.querySelector(".box").innerHTML=`<button class="play btn " event="click"><i class="fas fa-play"></i></button>`;
+   
+// });
+document.querySelector(".btn2").addEventListener("click",function()
+{
+    let c=document.querySelector(".btn2");
+    if(c.classList.contains("play"))
+    {
+    document.querySelector(".btn2").classList.add("pause");
+    document.querySelector(".btn2").innerHTML=`<i class="fas fa-pause"></i>`;
+    document.querySelector(".btn2").classList.remove("play");
+    }
+    else{
+        document.querySelector(".btn2").classList.add("play");
+        document.querySelector(".btn2").innerHTML=`<i class="fas fa-play"></i>`;
+        document.querySelector("btn2").classList.remove("pause");
+    }
+    
+});
+
