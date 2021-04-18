fbf410c66ae43dbe8ff9c8ffa03553764c3db2d2
Saikat <saikatmohanta43434@gmail.com>
2021-04-17 22:23:25 -0400

    Removing unnecessary lines

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var i = 0;
 var correct = 0;
 var wrong = 0;
+
 document.addEventListener("keypress", function (event) {
     var key = event.key;
-    var audio = new Audio("audio/keypress.mp3");
-    audio.play();
-
-    var text = document.querySelectorAll(".letter")[i].innerText.charAt(0);
-    // var text2 = document.querySelectorAll(".letter")[i].innerText;
+   
+    
+        var audio = new Audio("audio/keypress.mp3");
+        audio.play();
 
     
-    // console.log(text2);
+
+
+    var text = document.querySelectorAll(".letter")[i].innerText.charAt(0);
+    
 
     if (key == text) {
         document.querySelectorAll(".letter")[i].classList.add("color_white");
@@ -35,18 +38,7 @@ document.addEventListener("keypress", function (event) {
         console.log("The no. of wrong typed letter are:"+wrong);
     }
 });
-// document.querySelector(".play").addEventListener("click",function()
-// {
 
-//     document.querySelector(".box").innerHTML=`<button class="pause btn " event="click"><i class="fas fa-pause"></i></button>`;
-    
-// });
-// document.querySelector(".pause").addEventListener("click",function()
-// {
-    
-//     document.querySelector(".box").innerHTML=`<button class="play btn " event="click"><i class="fas fa-play"></i></button>`;
-   
-// });
 document.querySelector(".btn2").addEventListener("click",function()
 {
     let c=document.querySelector(".btn2");
